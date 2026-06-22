8723d300411eca1a93e963eb53308eab0f2a3ab4
gabrielramos02 <gabriekrp02ok@outlook.com>
2026-06-23 01:16:32 +0200

    added callbackquery and inline keyboard

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    Update,
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 import logging
 
-from api_request import get_from_url, get_team_info, get_teams_list
-from config import BOT_TOKEN
+from src.api_request import get_from_url, get_team_info, get_teams_list
+from src.config import BOT_TOKEN
 from database.manager import test_database
 
 logging.basicConfig(
@@ -22,38 +35,45 @@ async def list(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 chat_id=update.effective_chat.id, text="No teams found."
             )
         return
-    # TODO: add option to get info by id, and add more info to the list
+    # TODO:  add more info to the list
     mensaje_final = "\n".join(
         f"• {team["displayName"]} /i_{team["id"]}" for team in response
     )
 
-    if update.effective_chat:
-        _ = await context.bot.sendMessage(
-            chat_id=update.effective_chat.id, text=f"Teams:\n{mensaje_final}"
-        )
+    if update.message:
+        _ = await update.message.reply_text(mensaje_final)
 
-    ## TODO: add comand to get team info by id
 async def team_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.text:
         team_id = update.message.text.split("_")[1]
         response = await get_team_info(team_id)
-        print(response)
         mensaje_final = f"Team Info:\nName: {response["displayName"]}\n"
-        if update.effective_chat:
-            _ = await context.bot.sendMessage(chat_id=update.effective_chat.id, text=mensaje_final)
+        keyboard = [[InlineKeyboardButton("Suscribe", callback_data=f"sub{team_id}")]]
 
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        
+        if update.message:
+            _ = await update.message.reply_text(mensaje_final, reply_markup=reply_markup)
 
+async def sub_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    if query and query.data:
+        team_id = query.data[3:]  
+        # TODO: Add logic to subscribe the user to the team using the team_id
+        _ = await query.answer()  
+        _ = await query.edit_message_text(text=f"You have subscribed to team {team_id}.")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(str(BOT_TOKEN)).build()
 
-    #start_handler = CommandHandler("start", start)
+    # start_handler = CommandHandler("start", start)
     list_handler = CommandHandler("list", list)
     team_info_handler = MessageHandler(filters.Regex(r"^/i_\d+$"), team_info)
 
-    #application.add_handler(start_handler)
+    # application.add_handler(start_handler)
     application.add_handler(list_handler)
     application.add_handler(team_info_handler)
+    application.add_handler(CallbackQueryHandler(sub_button))
 
     application.run_polling()
