62f8f7380b88cb1862b6cb97cde7dc3a0d06e27f
gabrielramos02 <gabriekrp02ok@outlook.com>
2026-06-23 10:03:45 +0200

    added subscription logic

--- database/manager.py
+++ database/manager.py
@@ -1,15 +1,13 @@
 from datetime import datetime
 
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Subscription(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     chat_id: str = Field(nullable=False)
-    team_id : int = Field(nullable=False)
-    team_name : str = Field(nullable=False)
-    created_at: datetime  = Field(default_factory=lambda: datetime.now(), nullable=False)
-
+    team_id: int = Field(nullable=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(), nullable=False)
 
 
 engine = create_engine("sqlite:///database/database.db")
@@ -17,6 +15,23 @@ engine = create_engine("sqlite:///database/database.db")
 
 SQLModel.metadata.create_all(engine)
 
+
 async def test_database():
     with Session(engine) as session:
         session.commit()
+
+
+async def add_subscription(chat_id: str, team_id: int ):
+    with Session(engine) as session:
+        subscription = Subscription(
+            chat_id=chat_id,
+            team_id=team_id,
+        )
+        session.add(subscription)
+        session.commit()
+
+async def get_subscription_for_team(chat_id: str, team_id: int) -> bool:
+    with Session(engine) as session:
+        statement = select(Subscription).where(Subscription.chat_id == chat_id, Subscription.team_id == team_id)
+        results = session.exec(statement)
+        return  results.first() is not None
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import logging
 
 from src.api_request import get_from_url, get_team_info, get_teams_list
 from src.config import BOT_TOKEN
-from database.manager import test_database
+from database.manager import get_subscription_for_team, test_database, add_subscription
 
 logging.basicConfig(
     level=logging.INFO,
@@ -43,25 +43,60 @@ async def list(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
         _ = await update.message.reply_text(mensaje_final)
 
+
 async def team_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.text:
         team_id = update.message.text.split("_")[1]
         response = await get_team_info(team_id)
-        mensaje_final = f"Team Info:\nName: {response["displayName"]}\n"
-        keyboard = [[InlineKeyboardButton("Suscribe", callback_data=f"sub{team_id}")]]
+        display_name = response.get("displayName", "Unknown Team")
+        mensaje_final = f"Team Info:\nName: {display_name}\n"
+        suscribed = await get_subscription_for_team(
+            chat_id=str(update.message.chat.id), team_id=int(team_id)
+        )
+        sub_icon = suscribed and "✅" or "❌"
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    f"Suscribed {sub_icon}", callback_data=f"sub{team_id}"
+                )
+            ]
+        ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
-        
+
         if update.message:
-            _ = await update.message.reply_text(mensaje_final, reply_markup=reply_markup)
+            _ = await update.message.reply_text(
+                mensaje_final, reply_markup=reply_markup
+            )
+
 
 async def sub_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     if query and query.data:
-        team_id = query.data[3:]  
-        # TODO: Add logic to subscribe the user to the team using the team_id
-        _ = await query.answer()  
-        _ = await query.edit_message_text(text=f"You have subscribed to team {team_id}.")
+        team_id = query.data[3:]
+        if query.message and query.message.chat:
+            await add_subscription(
+                chat_id=str(query.message.chat.id),
+                team_id=int(team_id),
+            )
+        sub_icon = "✅" 
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    f"Suscribed {sub_icon}", callback_data=f"sub{team_id}"
+                )
+            ]
+        ]
+
+        if query.message.text:
+            old_text = query.message.text
+        else:
+            old_text = "Team Info"
+        _ = await query.answer(text= "Subscription added!", show_alert=True)
+        _ = await query.edit_message_text(
+                text = old_text,reply_markup=InlineKeyboardMarkup(keyboard)
+
+        )
 
 
 if __name__ == "__main__":
